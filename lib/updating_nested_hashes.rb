8eabacd0a2a402677027f42c65c24fbb29bfb969
Jonathan Yu <jonathan.yu.jwy2@yale.edu>
2021-06-02 21:05:13 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -61,11 +61,8 @@ def add_characters
    # Code your solution here:
 
   epic_tragedy[:additional_characters] = []
-  epic_tragedy[:additional_characters] << prince
-  epic_tragedy[:additional_characters] << apothecary
-
-
-
+  epic_tragedy[:additional_characters] << { name: "Prince Escalus" }
+  epic_tragedy[:additional_characters] << { name: "Apothecary" }
 
    # After your code, the following line will return the altered hash
    epic_tragedy
